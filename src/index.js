03373c501d3e37dfc08fdef6448c2a81549b32a1
Virgil <lunguvirgil3000@gmail.com>
2022-09-13 12:32:42 +0300

    add opacity to img

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
-
+import './style.css'
 function component() {
   const element = document.createElement('div');
+  element.className = 'content';
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = `<img src="./chainesh.jpg" a="chineserestaurant">
   <h3>The Qingdao restaurant offers customers great food for low prices, with a wide variety of Asian dishes.</h3>`
