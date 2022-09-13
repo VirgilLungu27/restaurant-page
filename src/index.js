f501c7ec942c674b69c1b7cc44c533730f8b1b0f
Virgil <lunguvirgil3000@gmail.com>
2022-09-13 13:00:46 +0300

    create two initial divs with main info. To add main and contact pageS

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ function component() {
   const element = document.createElement('div');
   element.className = 'content';
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = `<img src="./chainesh.jpg" a="chineserestaurant">
-  <h3>The Qingdao restaurant offers customers great food for low prices, with a wide variety of Asian dishes.</h3>`
+  element.innerHTML = `
+  <img src="./chainesh.jpg" a="chineserestaurant">
+  <div class="welcome"><h1>Welcome to Qingdao.</q1></div>
+  <div class="restaurant"><h3>The Qingdao restaurant offers customers great food for affordable prices, with a wide variety of Asian dishes.</h3></div>
+  <div class="chef"><h3>We offer food prepared by experienced chefs, with ingredients outsourced from responsible sources.</div>`
   return element;
 }
 
