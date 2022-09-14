08684f126b87210bcbac9e2ce07183dde6bd9add
Virgil <lunguvirgil3000@gmail.com>
2022-09-14 10:25:38 +0300

    add menu and contact buttons

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ function component() {
   element.className = 'content';
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = `
+  <div class="hero">
   <img src="./chainesh.jpg" a="chineserestaurant">
+  <button id="menu">Menu</button>
+  <button id="contact">Contact</button>
+  </div>
   <div class="welcome"><h1>Welcome to Qingdao.</q1></div>
   <div class="restaurant"><h3>The Qingdao restaurant offers customers great food for affordable prices, with a wide variety of Asian dishes.</h3></div>
   <div class="chef"><h3>We offer food prepared by experienced chefs, with ingredients outsourced from responsible sources.</h3></div>
