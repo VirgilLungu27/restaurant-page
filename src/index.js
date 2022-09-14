9019b4aa88bf202d4b40be119b206cad90c8f450
Virgil <lunguvirgil3000@gmail.com>
2022-09-14 09:55:03 +0300

    Add location

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function component() {
   <div class="restaurant"><h3>The Qingdao restaurant offers customers great food for affordable prices, with a wide variety of Asian dishes.</h3></div>
   <div class="chef"><h3>We offer food prepared by experienced chefs, with ingredients outsourced from responsible sources.</h3></div>
   <div class="militari"><h3>Discover our shop in Militari and enjoy high-quality Chinese food.</h3></div>
-  <div class="hours"><h4>Monday - Thursday: 10:00 - 23:00\n Friday - Saturday: 10:00 - 02:00\n Sunday: 14:00 - 00:00</h4></div>
+  <div class="hours"><h4>Monday - Thursday: 10:00 - 23:00\n Friday - Saturday: 10:00 - 02:00\n Sunday: 14:00 - 00:00 <h5>306 Splaiul Independentei \n Bucuresti</h5></div>
   `
   return element;
 }
