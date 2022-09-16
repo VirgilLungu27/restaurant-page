dc9af738cfdf707b2ce5614a2cea412c65e68b3d
Virgil <lunguvirgil3000@gmail.com>
2022-09-16 09:49:13 +0300

    Add same image while keeping css opacity

--- src/menu.js
+++ src/menu.js
@@ -3,7 +3,10 @@ import _ from 'lodash';
 function menuLoad() { 
     const element = document.createElement('div');
     element.className = 'content';
-    element.innerHTML = `<img src="./food-pattern.jpg" a="chineserestaurant">` 
+    element.innerHTML = `
+    <div class="hero">
+        <img src="./food-pattern.jpg" a="chineserestaurant">
+    </div>` 
     return element;
 }
 
