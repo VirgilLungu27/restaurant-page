50427574814997f489936d08cf5a03a061cd9f51
Virgil <lunguvirgil3000@gmail.com>
2022-09-16 14:40:13 +0300

    add beverages sides and main course

--- src/menu.js
+++ src/menu.js
@@ -7,8 +7,19 @@ function menuLoad() {
     <div class="menuhero">
         <img src="./food-pattern.jpg" a="chineserestaurant">
     </div>
-    <div class="menumenu">
-        <h3>Menu</h3>
+    <div class="menupage">
+        <div class="menumenu">
+            <h3>Menu</h3>
+        </div>
+        <div class="menubeverages">
+            <h3>Beverages</h3>
+        </div>
+        <div class="menusides">
+            <h3>Sides</h3>
+        </div>
+        <div class="menumaincourse">
+            <h3>Main Course</h3>
+        </div>
     </div>
     ` 
     return element;
