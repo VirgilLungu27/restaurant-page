4502f88855377fca20784b51e0d0bc2261e74b3b
Virgil <lunguvirgil3000@gmail.com>
2022-09-16 15:03:38 +0300

    Encapsulate each beverage into its own div

--- src/menu.js
+++ src/menu.js
@@ -13,7 +13,16 @@ function menuLoad() {
         </div>
         <div class="menubeverages">
             <h3>Beverages</h3>
-            <h4>Coca Cola 500ml <span></span> 7 lei</h4>
+
+            <div><h4>Coca Cola 500ml</h4><h4>7 lei</h4></div>
+            <div><h4>Fanta 500ml</h4><h4> 7 lei</h4></div>
+            <div><h4>Sprite 500ml</h4><h4> 7 lei</h4></div>
+            <h4>Dorna 500ml <span></span> 6 lei</h4>
+            <h4>Borsec 500ml <span></span> 6 lei</h4>
+            <h4>Ciuc 500ml <span></span> 8 lei</h4>
+            <h4>Staropramen 500ml <span></span> 9 lei</h4>
+            <h4>Heineken 400ml <span></span> 10 lei</h4>
+
         </div>
         <div class="menusides">
             <h3>Sides</h3>
