68f70e1d35de467c20ba649faa291cd575e0923e
Virgil <lunguvirgil3000@gmail.com>
2022-09-16 15:27:31 +0300

    add sides css

--- src/menu.js
+++ src/menu.js
@@ -22,10 +22,18 @@ function menuLoad() {
             <div><h4>Ciuc 500ml</h4><h4> 8 lei</h4></div>
             <div><h4>Staropramen 500ml</h4><h4> 9 lei</h4></div>
             <div><h4>Heineken 400ml</h4><h4> 10 lei</h4></div>
+            <div><h4>Feteasca Alba 750ml</h4><h4> 65 lei</h4></div>
+            <div><h4>Cabernet 750ml</h4><h4> 71 lei</h4></div>
+            <div><h4>Finlandia 25ml</h4><h4> 10 lei</h4></div>
 
         </div>
         <div class="menusides">
             <h3>Sides</h3>
+                <div><h4>4 slices of bread</h4><h4>2 lei</h4></div>
+                <div><h4>150g French Fries</h4><h4>5 lei</h4></div>
+                <div><h4>20g Mayonnaise</h4><h4>2 lei</h4></div>
+                <div><h4>20g Sweet Ketchup</h4><h4>2 lei</h4></div>
+                <div><h4>20g Spicy Ketchup</h4><h4>2 lei</h4></div>
         </div>
         <div class="menumaincourse">
             <h3>Main Dishes</h3>
