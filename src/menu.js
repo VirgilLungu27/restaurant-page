281cc1cf0849ee547b19eed9c8430a2acf7d21c9
Virgil <lunguvirgil3000@gmail.com>
2022-09-16 14:27:05 +0300

    add menu button on menu js css

--- src/menu.js
+++ src/menu.js
@@ -4,9 +4,13 @@ function menuLoad() {
     const element = document.createElement('div');
     element.className = 'content';
     element.innerHTML = `
-    <div class="hero">
+    <div class="menuhero">
         <img src="./food-pattern.jpg" a="chineserestaurant">
-    </div>` 
+    </div>
+    <div class="menumenu">
+        <h3>Menu</h3>
+    </div>
+    ` 
     return element;
 }
 
